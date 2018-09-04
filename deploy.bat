892d90f7c33ac857594aa2bca1de95fedb69a55a
330吴彦祖 <windshear222@outlook.com>
2018-09-04 17:51:04 +0800

    'deploy'

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,7 @@
 cd ./docs/.vuepress/dist/
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:330wuyanzu/AC.git origin:gh-pages
+git push -f git@github.com:330wuyanzu/AC.git gh-pages:gh-pages
 cd ../
 cd ../
 cd ../
