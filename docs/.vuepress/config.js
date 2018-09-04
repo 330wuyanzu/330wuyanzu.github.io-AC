11a992aff05d04a3e91bd43d10e4e6ad7414422d
330吴彦祖 <windshear222@outlook.com>
2018-09-04 19:52:47 +0800

    'deploy'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,16 +3,19 @@ module.exports = {
     title: 'AIRBUS Note',
     description: '空客常见机型学习笔记',
     themeConfig: {
+      editLinks: true,
+      editLinkText: '编辑此页',
+      lastUpdated: '上次更新',
       nav: [
-        { text: 'Home', link: '/' },
-        { text: 'A320FM', link: '/A320FM/' },
-        { text: 'A330', link: '/A330/' },
-        { text: 'TBA', link: 'http://www.tibetairlines.com.cn/'},
+        { text: 'Home',         link: '/' },
+        { text: 'A320FM',       link: '/A320FM/' },
+        { text: 'A330',         link: '/A330/' },
+        { text: 'TBA',          link: 'http://www.tibetairlines.com.cn/'},
         { text: 'Airbus World', link: 'http://www.airbusworld.com'},
-        { text: 'AIRBUS', link: 'https://www.airbus.com'},
-        { text: 'AIRBUS China', link: 'http://www.airbus.com.cn'}
+        { text: 'AIRBUS',       link: 'https://www.airbus.com'},
+        { text: 'AIRBUS China', link: 'http://www.airbus.com.cn'},
+        { text: 'Github',       link: 'https://github.com/330wuyanzu/330wuyanzu.github.io-AC'}
       ],
-      sidebar: 'auto',
-      lastUpdated: '上次更新'
+      sidebar: 'auto'
     },
   }
